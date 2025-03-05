e698f8c94b007f4daaf287df21070d0a38fe8f70
Shoaib Azhar <74708002+1shoaibazhar@users.noreply.github.com>
2025-03-05 15:31:17 +0100

    Trained a simple chat bot

--- training.py
+++ training.py
@@ -64,7 +64,7 @@ sgd = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 model.fit(trainX, trainY, epochs=200, batch_size=5, verbose=1)
-model.save('chatbot_model.h5')
+model.save('chatbot_model.keras')
 print('Done')
 
 
